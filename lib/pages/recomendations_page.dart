6d812dcbb39aaea430db13f03ab6edcbdee93374
Фаиз <galimow63@gmail.com>
2022-02-07 21:07:40 +0400

    RecomendationsPage managing

--- lib/pages/recomendations_page.dart
+++ lib/pages/recomendations_page.dart
@@ -3,13 +3,34 @@ import 'package:flutter/material.dart';
 class RecomendationsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final String _pageName = 'Recommendations';
+    final _contextTheme = Theme.of(context);
+    final _mediaQuery = MediaQuery.of(context);
+
+    final appBar = AppBar(
+      //TODO
+      leading: const IconButton(icon: Icon(Icons.menu)),
+      backgroundColor: _contextTheme.canvasColor,
+      elevation: 0,
+    );
+
+    final body = SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Text(
+              _pageName,
+              style: _contextTheme.textTheme.headline1,
+            ),
+          )
+        ],
+      ),
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
+      appBar: appBar,
+      body: body,
     );
   }
 }
