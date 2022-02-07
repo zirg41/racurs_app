420682372ad6477137a0eff2d80df03f7418ca24
Фаиз <galimow63@gmail.com>
2022-02-07 21:07:10 +0400

    Theming

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,18 @@ class RacursApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Racurs App',
+      theme: ThemeData(
+        canvasColor: Color.fromRGBO(250, 250, 250, 1),
+        fontFamily: "Montserrat",
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline1: TextStyle(
+                color: Color.fromRGBO(20, 20, 20, 1),
+                fontFamily: "Montserrat",
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
       home: RecomendationsPage(),
     );
   }
