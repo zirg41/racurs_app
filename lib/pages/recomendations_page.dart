d4a8870794c6c50771f0c33bab5c3952b2226ccd
Фаиз <galimow63@gmail.com>
2022-02-07 19:53:44 +0400

    RecomendationsPage() added

---
+++ lib/pages/recomendations_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecomendationsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Center(
+        child: Text('Widget Playground!'),
+      ),
+    );
+  }
+}
