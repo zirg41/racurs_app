2dd8ff6a78501aa965543a893ff0bc064e9d33e2
Фаиз <galimow63@gmail.com>
2022-02-08 21:26:32 +0400

    publication widg created

---
+++ lib/widgets/publication.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PublicationWidget extends StatelessWidget {
+  const PublicationWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
