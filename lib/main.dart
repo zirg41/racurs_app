671953b8bf56f90ff7b2d3d3511a5ea9aa41ce68
Фаиз <galimow63@gmail.com>
2022-06-27 21:40:08 +0400

    rebase to default app

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '/providers/publication_provider.dart';
-import '/providers/publications_provider.dart';
-import '/providers/user_provider.dart';
-import '/providers/users_provider.dart';
+void main() {
+  runApp(MyApp());
+}
 
-import '/pages/feed_page.dart';
-import '/pages/navigation_page.dart';
-import '/pages/recomendations_page.dart';
-
-void main() => runApp(RacursApp());
-
-class RacursApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var themeData = ThemeData(
-      canvasColor: Color.fromRGBO(250, 250, 250, 1),
-      fontFamily: "Montserrat",
-      textTheme: ThemeData.light().textTheme.copyWith(
-            headline1: TextStyle(
-              color: Color.fromRGBO(20, 20, 20, 1),
-              fontFamily: "Montserrat",
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
-            bodyText1: TextStyle(
-              color: Color.fromRGBO(20, 20, 20, 1),
-              fontFamily: "Montserrat",
-              fontSize: 16,
-              //fontWeight: FontWeight.bold,
-            ),
-            subtitle1: TextStyle(
-              color: Color.fromRGBO(20, 20, 20, 1),
-              fontFamily: "Arial",
-              fontSize: 17,
-              //fontWeight: FontWeight.bold,
-            ),
-          ),
-    );
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => Publications(),
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Empty Material App'),
         ),
-        ChangeNotifierProvider(
-          create: (context) => Publication(),
+        body: const Center(
+          child: Text('EmptyPage'),
         ),
-        ChangeNotifierProvider(
-          create: (context) => Users(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => User(),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'Racurs App',
-        theme: themeData,
-        home: NavigationPage(),
-        routes: {
-          HomePage.routeName: (ctx) => HomePage(),
-          FeedPage.routeName: (ctx) => FeedPage(),
-        },
       ),
     );
   }
