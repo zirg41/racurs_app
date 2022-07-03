2bbe7f1d7dc2e8ce1a54deac84bbb1adb89842ba
Фаиз <galimow63@gmail.com>
2022-07-03 22:46:19 +0400

    UniqueId added

---
+++ lib/domain/core/unique_id.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:uuid/uuid.dart';
+
+import 'value_failure.dart';
+import 'value_object.dart';
+
+class UniqueId extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory UniqueId() {
+    return UniqueId._(
+      right(const Uuid().v1()),
+    );
+  }
+
+  factory UniqueId.fromUniqueString(String input) {
+    return UniqueId._(right(input));
+  }
+  const UniqueId._(this.value);
+}
