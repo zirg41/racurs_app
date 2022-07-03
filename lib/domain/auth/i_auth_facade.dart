5297b8b5f6826c9a3c1aa9063d55b3b852a272bd
Фаиз <galimow63@gmail.com>
2022-07-03 17:06:01 +0400

    IAuthFacade added

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+
+import 'auth_failure.dart';
+import 'value_objects.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword(
+    Email email,
+    Password password,
+  );
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword(
+    Email email,
+    Password password,
+  );
+  Future<Either<AuthFailure, Unit>> signInWithApple();
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+  // Future<Option<User>> getSignedInUser();
+  Future<void> signOut();
+}
