35b49ed7af0c4f5f710ea1f1e71efc60e449bca1
Фаиз <galimow63@gmail.com>
2022-07-17 22:31:46 +0400

    signUpPage refactoring

--- lib/presentation/pages/auth/sign_up/sign_up_page.dart
+++ lib/presentation/pages/auth/sign_up/sign_up_page.dart
@@ -1,17 +1,63 @@
-import 'widgets/sign_up_form.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../application/auth/auth_bloc.dart';
+import '../../../../../application/auth/sign_in_form/sign_in_form_bloc.dart';
 import '../../../../application/auth/sign_in_form/sign_in_form_bloc.dart';
-import '../../../../injection.dart';
+import '../../../routes/router.gr.dart';
+import '../widgets/already_have_an_account.dart';
+import '../widgets/app_logo.dart';
+import '../widgets/apple_google_sign_in_button.dart';
+import '../widgets/auth_failure_snackbar.dart';
+import '../widgets/background.dart';
+import '../widgets/devider.dart';
+import 'widgets/sign_up_form.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SignUpForm(),
+    return Scaffold(
+      body: BlocListener<SignInFormBloc, SignInFormState>(
+        listener: (ctx, state) {
+          state.authFailureOrSuccessOption.fold(
+            () => null,
+            (either) => either.fold(
+              (f) => showAuthFailureSnackBar(context: ctx, failure: f),
+              (_) {
+                ctx.router.replace(const HomeRoute());
+                ctx.read<AuthBloc>().add(const AuthEvent.authCheckReqested());
+              },
+            ),
+          );
+        },
+        child: Stack(
+          children: [
+            const SignInBackground(),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ListView(
+                  children: [
+                    const RacursLogoText(),
+                    const SignUpForm(),
+                    const CustomDevider(),
+                    const EitherAppleOrGoogleSignInButton(),
+                    const CustomDevider(),
+                    AlreadyHaveAnAccountButton(
+                      pushToSignUpPageFunction: () {
+                        context.router.replace(const SignInRoute());
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/presentation/pages/auth/sign_up/widgets/sign_up_form.dart
+++ lib/presentation/pages/auth/sign_up/widgets/sign_up_form.dart
@@ -1,202 +1,149 @@
-import 'package:auto_route/auto_route.dart';
-import '../../../../../application/auth/auth_bloc.dart';
-import '../../../../../application/auth/sign_in_form/sign_in_form_bloc.dart';
-import '../../widgets/already_have_an_account.dart';
-import '../../widgets/apple_google_sign_in_button.dart';
-import '../../widgets/dont_have_an_account_button.dart';
-import '../../../../routes/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:parse_server_sdk_flutter/generated/i18n.dart';
 
+import '../../../../../application/auth/sign_in_form/sign_in_form_bloc.dart';
 import '../../messages.dart';
-import '../../widgets/background.dart';
 
-class SignUpForm extends StatelessWidget {
+class SignUpForm extends StatefulWidget {
   const SignUpForm({Key? key}) : super(key: key);
 
   @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  bool _passwordVisible = false;
+
+  @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
     final contextTheme = Theme.of(context);
 
-    final customDevider = Column(children: const [
-      SizedBox(height: 15),
-      Divider(),
-      SizedBox(height: 15)
-    ]);
-
-    return BlocConsumer<SignInFormBloc, SignInFormState>(
-      listener: (context, state) {
-        print(state);
-        state.authFailureOrSuccessOption.fold(
-          () => null,
-          (either) => either.fold(
-            (failure) {
-              final scafMessanger = ScaffoldMessenger.of(context);
-              scafMessanger.clearSnackBars();
-              scafMessanger.showSnackBar(
-                SnackBar(
-                  content: Text(
-                    failure.map(
-                      cancelledByUser: (_) => '',
-                      serverError: (_) => SERVER_ERROR_MESSAGE,
-                      usernameAlredyInUse: (_) => USERNAME_ALREADY_IN_USE,
-                      invalidUsernameAndPasswordCombination: (_) =>
-                          INVALID_USERNAME_AND_PASSWORD_COMBINATION,
+    return BlocBuilder<SignInFormBloc, SignInFormState>(
+      builder: (context, state) {
+        return Form(
+          autovalidateMode: state.validateMode,
+          child: Column(
+            children: [
+              //@
+              //@ USERNAME TEXT FIELD
+              TextFormField(
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                  labelText: CREATE_NICKNAME_TEXT,
+                  labelStyle: contextTheme.textTheme.labelMedium,
+                ),
+                autocorrect: false,
+                onChanged: (value) {
+                  context
+                      .read<SignInFormBloc>()
+                      .add(SignInFormEvent.usernameChanged(value));
+                },
+                validator: (_) =>
+                    context.read<SignInFormBloc>().state.username.value.fold(
+                          (f) => f.maybeMap(
+                              orElse: () => null,
+                              shortUsername: (_) => SHORT_USERNAME),
+                          (_) => null,
+                        ),
+              ),
+              const SizedBox(height: 10),
+              //@
+              //@ PASSWORD FIELD
+              TextFormField(
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(
+                    Icons.lock,
+                    color: Colors.white70,
+                  ),
+                  labelText: CREATE_PASSWORD_TEXT,
+                  labelStyle: contextTheme.textTheme.labelMedium,
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _passwordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      color: contextTheme.colorScheme.outline,
                     ),
+                    onPressed: () {
+                      setState(() => _passwordVisible = !_passwordVisible);
+                    },
                   ),
                 ),
-              );
-            },
-            (_) {
-              context.router.navigate(const HomeRoute());
-              context.read<AuthBloc>().add(const AuthEvent.authCheckReqested());
-            },
-          ),
-        );
-      },
-      builder: (context, state) {
-        return Stack(
-          children: [
-            const SignInBackground(),
-            Center(
-              child: Form(
-                autovalidateMode: state.validateMode,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: ListView(
-                    children: [
-                      SizedBox(height: screenHeight * 0.08),
-                      Text(
-                        //@ APP NAME TEXT
-                        APP_LOGO_NAME,
-                        textAlign: TextAlign.center,
-                        style: contextTheme.textTheme.displayLarge,
-                      ),
-                      SizedBox(height: screenHeight * 0.08),
-                      TextFormField(
-                        //@ USERNAME TEXT FIELD
-                        style: const TextStyle(color: Colors.white),
-
-                        decoration: InputDecoration(
-                          prefixIcon: const Icon(
-                            Icons.email,
-                            color: Colors.white,
+                obscureText: !_passwordVisible,
+                autocorrect: false,
+                onChanged: (value) {
+                  context
+                      .read<SignInFormBloc>()
+                      .add(SignInFormEvent.passwordChanged(value));
+                },
+                validator: (_) =>
+                    context.read<SignInFormBloc>().state.password.value.fold(
+                          (f) => f.maybeMap(
+                            orElse: () => null,
+                            shortPassword: (_) => SHORT_PASSWORD,
                           ),
-                          labelText: CREATE_NICKNAME_TEXT,
-                          labelStyle: contextTheme.textTheme.labelMedium,
+                          (_) => null,
                         ),
-
-                        autocorrect: false,
-                        onChanged: (value) {
-                          context
-                              .read<SignInFormBloc>()
-                              .add(SignInFormEvent.usernameChanged(value));
-                        },
-                        validator: (_) => context
-                            .read<SignInFormBloc>()
-                            .state
-                            .username
-                            .value
-                            .fold(
-                              (f) => f.maybeMap(
-                                  orElse: () => null,
-                                  invalidEmail: (_) => INVALID_EMAIL),
-                              (_) => null,
-                            ),
-                      ),
-                      const SizedBox(height: 10),
-                      TextFormField(
-                        //@ PASSWORD FIELD
-                        style: const TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                          prefixIcon: const Icon(
-                            Icons.lock,
-                            color: Colors.white70,
-                          ),
-                          labelText: CREATE_PASSWORD_TEXT,
-                          labelStyle: contextTheme.textTheme.labelMedium,
-                        ),
-                        obscureText: true,
-                        autocorrect: false,
-                        onChanged: (value) {
-                          context
-                              .read<SignInFormBloc>()
-                              .add(SignInFormEvent.passwordChanged(value));
-                        },
-                        validator: (_) => context
-                            .read<SignInFormBloc>()
-                            .state
-                            .password
-                            .value
-                            .fold(
-                              (f) => f.maybeMap(
-                                orElse: () => null,
-                                shortPassword: (_) => SHORT_PASSWORD,
-                              ),
-                              (_) => null,
-                            ),
-                      ),
-                      const SizedBox(height: 10),
-                      TextFormField(
-                        //@ REPAET PASSWORD FIELD
-                        style: const TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                          prefixIcon: const Icon(
-                            Icons.lock,
-                            color: Colors.white70,
-                          ),
-                          labelText: REPEAT_PASSWORD_TEXT,
-                          labelStyle: contextTheme.textTheme.labelMedium,
-                        ),
-                        obscureText: true,
-                        autocorrect: false,
-                        onChanged: (value) {
-                          context.read<SignInFormBloc>().add(
-                              SignInFormEvent.repeatedPasswordChanged(value));
-                        },
-                        validator: (inputValue) {
-                          if (inputValue !=
-                              context
-                                  .read<SignInFormBloc>()
-                                  .state
-                                  .password
-                                  .value
-                                  .getOrElse(() => '')) {
-                            return PASSWORDS_MUST_BE_SAME;
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      TextButton(
-                        onPressed: () {
-                          context.read<SignInFormBloc>().add(
-                                const SignInFormEvent
-                                    .registerWithUsernameAndPasswordPressed(),
-                              );
-                        },
-                        child: const Text(
-                          CREATE_ACCOUNT_TEXT,
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        ),
-                      ),
-                      customDevider,
-                      const EitherAppleOrGoogleSignInButton(),
-                      customDevider,
-                      AlreadyHaveAnAccountButton(
-                        pushToSignUpPageFunction: () {
-                          context.router.replace(const SignInRoute());
-                        },
-                      )
-                    ],
+              ),
+              const SizedBox(height: 10),
+              //@
+              //@ REPAET PASSWORD FIELD
+              TextFormField(
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(
+                    Icons.lock,
+                    color: Colors.white70,
                   ),
+                  labelText: REPEAT_PASSWORD_TEXT,
+                  labelStyle: contextTheme.textTheme.labelMedium,
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _passwordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      color: contextTheme.colorScheme.outline,
+                    ),
+                    onPressed: () {
+                      setState(() => _passwordVisible = !_passwordVisible);
+                    },
+                  ),
+                ),
+                obscureText: !_passwordVisible,
+                autocorrect: false,
+                onChanged: (value) {
+                  context
+                      .read<SignInFormBloc>()
+                      .add(SignInFormEvent.repeatedPasswordChanged(value));
+                },
+                validator: (inputValue) {
+                  if (inputValue != state.password.value.getOrElse(() => '')) {
+                    return PASSWORDS_MUST_BE_SAME;
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () {
+                  context.read<SignInFormBloc>().add(
+                        const SignInFormEvent
+                            .registerWithUsernameAndPasswordPressed(),
+                      );
+                },
+                child: const Text(
+                  CREATE_ACCOUNT_TEXT,
+                  style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 10),
+            ],
+          ),
         );
       },
     );
