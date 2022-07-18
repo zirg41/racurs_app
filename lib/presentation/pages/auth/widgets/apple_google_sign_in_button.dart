ca10b1b30aebef55adb617fc2eebee76e98a2bfb
Фаиз <galimow63@gmail.com>
2022-07-18 21:10:33 +0400

    apple button draft button added

--- lib/presentation/pages/auth/widgets/apple_google_sign_in_button.dart
+++ lib/presentation/pages/auth/widgets/apple_google_sign_in_button.dart
@@ -1,5 +1,6 @@
 import '../messages.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
 
 class EitherAppleOrGoogleSignInButton extends StatelessWidget {
   const EitherAppleOrGoogleSignInButton({Key? key}) : super(key: key);
@@ -11,28 +12,58 @@ class EitherAppleOrGoogleSignInButton extends StatelessWidget {
     return Center(
       child: SizedBox(
         width: MediaQuery.of(context).size.width * 0.65,
-        child: ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-              primary: Colors.black,
-              onPrimary: Theme.of(context).colorScheme.onPrimary),
-          child: Row(
-            children: [
-              Image.asset(
-                'lib/presentation/assets/google_logo.png',
-                scale: 80,
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                  onPrimary: Theme.of(context).colorScheme.onPrimary),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'lib/presentation/assets/google_logo.png',
+                    scale: 80,
+                  ),
+                  const Spacer(),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 13, horizontal: 0),
+                    child: Text(
+                      SIGN_IN_WITH_GOOGLE,
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  ),
+                  const Spacer(),
+                ],
               ),
-              const Spacer(),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 13, horizontal: 0),
-                child: Text(
-                  SIGN_IN_WITH_GOOGLE,
-                  style: TextStyle(color: Colors.white, fontSize: 15),
+            ),
+            const SizedBox(height: 15),
+            if (Platform.isIOS)
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.black,
+                    onPrimary: Theme.of(context).colorScheme.onPrimary),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'lib/presentation/assets/apple_logo.png',
+                      scale: 1.4,
+                    ),
+                    const Spacer(),
+                    const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 13, horizontal: 0),
+                      child: Text(
+                        SIGN_IN_WITH_APPLE,
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                    ),
+                    const Spacer(),
+                  ],
                 ),
               ),
-              const Spacer(),
-            ],
-          ),
+          ],
         ),
       ),
     );
