837fde2763bd7994e86dbea0ebc576159367f9a2
Фаиз <galimow63@gmail.com>
2022-07-25 20:40:02 +0400

    deletePublication() method implemented

--- lib/infrastructure/publication/back4app_post_facade.dart
+++ lib/infrastructure/publication/back4app_post_facade.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
@@ -10,6 +12,14 @@ import '../../domain/publication/i_post_facade.dart';
 import '../../domain/publication/post_failure.dart';
 import '../../domain/publication/publication.dart';
 import '../../domain/publication/value_objects.dart';
+
+const String publicationApiClassName = 'Publication';
+const String userApiName = 'user';
+const String imageApiName = 'image';
+const String titleApiName = 'title';
+const String pubIdApiName = 'pubId';
+const String locationApiName = 'location';
+const String dateApiName = 'createdDate';
 
 @Singleton(as: IPostFacade)
 class Back4AppPostFacade implements IPostFacade {
@@ -21,34 +31,77 @@ class Back4AppPostFacade implements IPostFacade {
     final parseFile = ParseFile(File(publication.imageFile.path));
     await parseFile.save();
 
-    final publicationToUpload = ParseObject('Publication')
-      ..set('user', currentUser)
-      ..set('image', parseFile)
-      ..set<String>('title', publication.title)
-      ..set<String>('pubId', publication.id.getOrCrash())
-      ..set<Map<String, dynamic>>('location', publication.location.toMap())
-      ..set<DateTime>('createdDate', publication.createdDate);
+    final publicationToUpload = ParseObject(publicationApiClassName)
+      ..set(userApiName, currentUser)
+      ..set(imageApiName, parseFile)
+      ..set<String>(titleApiName, publication.title)
+      ..set<String>(pubIdApiName, publication.id.getOrCrash())
+      ..set<Map<String, dynamic>>(locationApiName, publication.location.toMap())
+      ..set<DateTime>(dateApiName, publication.createdDate);
 
-    final response = await publicationToUpload.save();
+    final apiResponse = await publicationToUpload.save();
 
-    if (response.success) {
+    if (apiResponse.success) {
       return right(unit);
     } else {
       // TODO add connection checker which returns noInternet PostFailure
-      return left(PostFailure.serverError(response.error?.message));
+      // TODO add failure uninons for popular status codes (404, 500 ...)
+      return left(PostFailure.serverError(apiResponse.error?.message));
     }
   }
 
   @override
-  Future<Either<PostFailure, Unit>> deletePublication(UniqueId id) {
-    // TODO: implement deletePublication
-    throw UnimplementedError();
+  Future<Either<PostFailure, Unit>> deletePublication(UniqueId id) async {
+    final pubToDeleteInServer = ParseObject(publicationApiClassName)
+      ..objectId = id.getOrCrash();
+
+    final apiResponse = await pubToDeleteInServer.delete();
+
+    if (apiResponse.success) {
+      return right(unit);
+    } else {
+      // TODO add connection checker which returns noInternet PostFailure
+      // TODO add failure uninons for popular status codes (404, 500 ...)
+      return left(PostFailure.serverError(apiResponse.error?.message));
+    }
   }
 
   @override
-  Future<Either<PostFailure, List<Publication>>> getAllPublications() {
-    // TODO: implement getAllPublications
-    throw UnimplementedError();
+  Future<Either<PostFailure, List<Publication>>> getAllPublications() async {
+    var query = QueryBuilder<ParseObject>(ParseObject(publicationApiClassName));
+
+    final apiResponse = await query.query();
+
+    print('getAllPublications() apiResponse: $apiResponse');
+
+    if (apiResponse.success && apiResponse.results != null) {
+      return right(
+        (apiResponse.results as List<ParseObject>).map(
+          (element) => Publication(
+            id: UniqueId.fromUniqueString(
+              element.get(pubIdApiName).toString(),
+            ),
+            user: User(
+              id: UniqueId.fromUniqueString(
+                element.get(userApiName).objectId,
+              ),
+              username: element.get<ParseUser>(userApiName)!.username as String,
+            ),
+            imageFile: element.get<File>(imageApiName)!,
+            location: GeoLocation.fromMap(
+              element.get<Map<String, dynamic>>(locationApiName)
+                  as Map<String, double>,
+            ),
+            createdDate: element.get<DateTime>(dateApiName) as DateTime,
+            title: element.get<String>(titleApiName) as String,
+          ),
+        ) as List<Publication>,
+      );
+    } else {
+      // TODO add connection checker which returns noInternet PostFailure
+      // TODO add failure uninons for popular status codes (404, 500 ...)
+      return left(PostFailure.serverError(apiResponse.error?.message));
+    }
   }
 
   @override
