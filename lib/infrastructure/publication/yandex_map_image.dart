4d7f8761fa5c2a82f978c23c46486036771f57e3
Фаиз <galimow63@gmail.com>
2022-07-30 14:11:02 +0400

    YandexImageMapProvider injectable error fixed

--- lib/infrastructure/publication/yandex_map_image.dart
+++ lib/infrastructure/publication/yandex_map_image.dart
@@ -4,7 +4,7 @@ import 'package:injectable/injectable.dart';
 import 'package:racurs_app/domain/publication/i_map_image_provider.dart';
 import 'package:racurs_app/domain/publication/value_objects.dart';
 
-@injectable
+@Singleton(as: IStaticMapImageProvider)
 class YandexImageMapProvider implements IStaticMapImageProvider {
   @override
   File getStaticMapImageByLocation(GeoLocation location) {
