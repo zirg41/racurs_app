050e9b23eececfaf0b348d02997991436473d163
Фаиз <galimow63@gmail.com>
2022-07-30 14:19:50 +0400

    getPublicationFromParseObject method added

--- lib/infrastructure/publication/back4app_post_facade.dart
+++ lib/infrastructure/publication/back4app_post_facade.dart
@@ -12,7 +12,8 @@ import '../../domain/publication/publication.dart';
 import '../../domain/publication/value_objects.dart';
 
 const String publicationApiClassName = 'Publication';
-const String userApiName = 'user';
+const String userPointerApiName = 'user';
+const String usernameApiName = 'username';
 const String imageApiName = 'image';
 const String titleApiName = 'title';
 const String pubIdApiName = 'pubId';
@@ -30,7 +31,8 @@ class Back4AppPostFacade implements IPostFacade {
     await parseFile.save();
 
     final publicationToUpload = ParseObject(publicationApiClassName)
-      ..set(userApiName, currentUser)
+      ..set(userPointerApiName, currentUser)
+      ..set(usernameApiName, publication.user.username)
       ..set(imageApiName, parseFile)
       ..set<String>(titleApiName, publication.title)
       ..set<String>(pubIdApiName, publication.id.getOrCrash())
@@ -71,7 +73,7 @@ class Back4AppPostFacade implements IPostFacade {
 
     final publicationToUpload = ParseObject(publicationApiClassName)
       ..objectId = updatingObjectId
-      ..set(userApiName, currentUser)
+      ..set(userPointerApiName, currentUser)
       ..set(imageApiName, parseFile)
       ..set<String>(titleApiName, publication.title)
       ..set<String>(pubIdApiName, publication.id.getOrCrash())
@@ -91,8 +93,24 @@ class Back4AppPostFacade implements IPostFacade {
 
   @override
   Future<Either<PostFailure, Unit>> deletePublication(UniqueId id) async {
+    late String pubObjectId;
+
+    var query = QueryBuilder<ParseObject>(ParseObject(publicationApiClassName))
+      ..whereEqualTo(
+        pubIdApiName,
+        id.getOrCrash(),
+      );
+
+    final queryResponse = await query.query();
+
+    if (queryResponse.success && queryResponse.results != null) {
+      pubObjectId = queryResponse.result.first.get('objectId').toString();
+    } else {
+      return left(PostFailure.serverError(queryResponse.error?.message));
+    }
+
     final pubToDeleteInServer = ParseObject(publicationApiClassName)
-      ..objectId = id.getOrCrash();
+      ..objectId = pubObjectId;
 
     final apiResponse = await pubToDeleteInServer.delete();
 
@@ -107,34 +125,24 @@ class Back4AppPostFacade implements IPostFacade {
 
   @override
   Future<Either<PostFailure, List<Publication>>> getAllPublications() async {
-    var query = QueryBuilder<ParseObject>(ParseObject(publicationApiClassName));
+    var query = QueryBuilder<ParseObject>(ParseObject(publicationApiClassName))
+      ..orderByDescending(dateApiName);
 
     final apiResponse = await query.query();
 
-    print('getAllPublications() apiResponse: $apiResponse');
+    // print(
+    //     'getAllPublications() apiResponse: ${apiResponse.results.toString()}');
 
     if (apiResponse.success && apiResponse.results != null) {
+      final parseObjects = apiResponse.results as List<ParseObject>;
+      // apiResult.map((element) => element.get(userPointerApiName).objectId);
+
       return right(
-        (apiResponse.results as List<ParseObject>).map(
-          (element) => Publication(
-            id: UniqueId.fromUniqueString(
-              element.get(pubIdApiName).toString(),
-            ),
-            user: User(
-              id: UniqueId.fromUniqueString(
-                element.get(userApiName).objectId,
-              ),
-              username: element.get<ParseUser>(userApiName)!.username as String,
-            ),
-            imageFile: element.get<File>(imageApiName)!,
-            location: GeoLocation.fromMap(
-              element.get<Map<String, dynamic>>(locationApiName)
-                  as Map<String, double>,
-            ),
-            createdDate: element.get<DateTime>(dateApiName) as DateTime,
-            title: element.get<String>(titleApiName) as String,
-          ),
-        ) as List<Publication>,
+        parseObjects.map(
+          (parseObject) {
+            return getPublicationFromParseObject(parseObject);
+          },
+        ).toList(),
       );
     } else {
       // TODO add connection checker which returns noInternet PostFailure
@@ -154,29 +162,11 @@ class Back4AppPostFacade implements IPostFacade {
 
     final apiResponse = await query.query();
 
-    print('getConcretePublication() apiResponse: $apiResponse');
+    // print('getConcretePublication() apiResponse: $apiResponse');
 
     if (apiResponse.success && apiResponse.results != null) {
-      final r = apiResponse.results as ParseObject;
-      return right(
-        Publication(
-          id: UniqueId.fromUniqueString(
-            r.get(pubIdApiName).toString(),
-          ),
-          user: User(
-            id: UniqueId.fromUniqueString(
-              r.get(userApiName).objectId,
-            ),
-            username: r.get<ParseUser>(userApiName)!.username as String,
-          ),
-          imageFile: r.get<File>(imageApiName)!,
-          location: GeoLocation.fromMap(
-            r.get<Map<String, dynamic>>(locationApiName) as Map<String, double>,
-          ),
-          createdDate: r.get<DateTime>(dateApiName) as DateTime,
-          title: r.get<String>(titleApiName) as String,
-        ),
-      );
+      final parseObject = apiResponse.results as ParseObject;
+      return right(getPublicationFromParseObject(parseObject));
     } else {
       // TODO add connection checker which returns noInternet PostFailure
       // TODO add failure uninons for popular status codes (404, 500 ...)
@@ -195,5 +185,27 @@ class Back4AppPostFacade implements IPostFacade {
       GeoLocation location) {
     // TODO: implement getNearbyPlaces
     throw UnimplementedError();
+  }
+
+  Publication getPublicationFromParseObject(ParseObject parseObject) {
+    return Publication(
+      id: UniqueId.fromUniqueString(
+        parseObject.get(pubIdApiName).toString(),
+      ),
+      user: User(
+        id: UniqueId.fromUniqueString(
+          parseObject.get(userPointerApiName).objectId,
+        ),
+        username: parseObject.get<String>(usernameApiName)!,
+      ),
+      imageFile: File.fromUri(
+        Uri(path: parseObject.get<ParseFileBase>(imageApiName)!.url!),
+      ),
+      location: GeoLocation.fromMap(
+        parseObject.get<Map<String, dynamic>>(locationApiName)!.cast(),
+      ),
+      createdDate: parseObject.get<DateTime>(dateApiName) as DateTime,
+      title: parseObject.get<String>(titleApiName) as String,
+    );
   }
 }
